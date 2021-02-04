def77ea7ac9c237e11189e3911347660beeca4e6
Fabian Riewe <fabianriewe00@gmail.com>
2021-02-04 09:48:46 +0100

    feat: added giveaway variables

--- config.ts
+++ config.ts
@@ -4,10 +4,10 @@ import {tokenAllocation} from "./arverify-distribution";
 
 
 const config: config = {
-  emission_period: 0, // E
-  time_interval: 0, // I
-  initial_emit_amount: 0, // A
-  decay_const: 0, // k
+  emission_period: 2592000, // E
+  time_interval: 86400, // I
+  initial_emit_amount: 96775, // A
+  decay_const: undefined, // k
   token_contract_id: COMMUNITY_ID,
   token_allocations: tokenAllocation(),
 };
